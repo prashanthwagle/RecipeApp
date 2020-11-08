876ba21801b0d8247a98d922a671a9b0a358168e
Prashanth <prashanthwagle360@gmail.com>
2020-11-08 09:51:24 +0530

    Removed MongoURI

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://codemonk:codemonk@cluster0-glrtp.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "prashanthIsAwesome"
-}
+  mongoURI: "mongoURI",
+  secretOrKey: "HireMe!",
+};
